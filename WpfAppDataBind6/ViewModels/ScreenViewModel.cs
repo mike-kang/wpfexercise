2bc6e87dbed2bf44b070810e7a2f0e9f205d793e
san <ksflex@gdis.co.kr>
2024-03-04 15:13:09 +0900

    add databind6 - viewmodel

---
+++ WpfAppDataBind6/ViewModels/ScreenViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppDataBind6.ViewModels
+{
+    public class ScreenViewModel : Notifier
+    {
+        private int _speed = 300;
+        public int Speed
+        {
+            get { return _speed; }
+            set
+            {
+                _speed = value;
+                OnPropertyChanged("Speed");
+            }
+        }
+    }
+
+    public class Notifier : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
